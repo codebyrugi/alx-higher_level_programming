e9a3db9ba63bf048585d441041d85d8874b2cb92
Rugi <rugibyrugi@gmail.com>
2022-12-07 21:18:57 -0800

    Corrected sorted linked list

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -8,12 +7,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX project
+ *
  */
 typedef struct listint_s
 {
-  int n;
-  struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -23,4 +22,3 @@ void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
-
